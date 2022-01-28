bfb36c9bdd1a1a5e83b4e331df716a21d2fab9df
dam450 <68165752+dam450@users.noreply.github.com>
2022-01-27 22:47:58 -0300

    responsiveness and ui
    ui corrections and responsiveness applied

--- js/owl/setup.js
+++ js/owl/setup.js
@@ -1,16 +1,25 @@
 $('.owl-carousel').owlCarousel({
+  stagePadding: 45,
   loop: true,
-  margin: 15,
   nav: false,
+  margin: 8,
+  center: false,
+  autoplay: false,
   responsive: {
     0: {
       items: 1
     },
+    450: {
+      items: 2
+    },
     600: {
       items: 3
     },
+    800: {
+      items: 4
+    },
     1000: {
-      items: 5
+      items: 6
     }
   }
 })
